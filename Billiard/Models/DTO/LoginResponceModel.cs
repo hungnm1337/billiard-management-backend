192097501aaba57804b259c5276d6870b2a58873
Nguyen Manh Hung <hungnm1337@gmail.com>
2025-05-20 02:18:38 +0700

    Login with JWT

---
+++ Billiard/Models/DTO/LoginResponceModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Billiard.Models.DTO
+{
+    public class LoginResponceModel
+    {
+        public string? Username { get; set; }
+        public string? AccessToken { get; set; }
+        public int ExpiresIn { get; set; }
+    }
+}
--- Billiard/Program.cs
+++ Billiard/Program.cs
@@ -1,4 +1,9 @@
 
+using Billiard.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
 namespace Billiard
 {
     public class Program
@@ -13,7 +18,30 @@ namespace Billiard
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+            builder.Services.AddAuthentication(
+                option =>
+                {
+                    option.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                    option.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme; ;
+                    option.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+                }
 
+                ).AddJwtBearer(option =>
+                {
+                    option.RequireHttpsMetadata = false;
+                    option.SaveToken = true;
+                    option.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidIssuer = builder.Configuration["Jwtconfig : Issuer"],
+                        ValidAudience = builder.Configuration["Jwtconfig : Audience"],
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwtconfig : Key"]!)),
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+                    };
+                });
+            builder.Services.AddAuthorization();
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
